285e1613cf6885264a2665b67b664ce16bc755a3
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2017-10-30 12:54:12 -0400

    Sauvegarde

--- target/classes/public/index.js
+++ target/classes/public/index.js
@@ -5,7 +5,6 @@ var votedisplay;
 var connecterror = "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>Erreur de connexion. Rafraîchissez la page.";
 var connectsuccess = "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>Vous êtes connecté.";
 var ws;
-//TODO Les lettres plus grosses
 //TODO Rétroaction du vote (dire pour lequel l'auditeur a voté)
 //TODO Changer ceci pour l'adresse IP du serveur injectée en jsp
 var address = 'ws://localhost:80/ws';
