636a9d09b62ac2017bfdb7385cb5a6524bba5d71
Mikaël Francoeur <mikael@mikaelfrancoeur.com>
2020-02-24 22:13:50 -0500

    fixed server in jar

--- src/main/java/server/ServerRunnableImpl.java
+++ src/main/java/server/ServerRunnableImpl.java
@@ -7,9 +7,6 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import util.PropLoader;
 
 import javax.servlet.DispatcherType;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.EnumSet;
 
 /**
@@ -28,28 +25,20 @@ public class ServerRunnableImpl implements ServerRunnable {
 
     @Override
     public void run() {
-        System.setProperty("org.eclipse.jetty.LEVEL", "INFO");
+        System.setProperty("org.eclipse.jetty.LEVEL", "DEBUG");
 
         ServerConnector connector = new ServerConnector(server);
-        //Changé pour marcher sous Linux
-        //connector.setPort(80);
         int port = Integer.parseInt(PropLoader.getMap().get("port"));
         connector.setPort(port);
         server.addConnector(connector);
 
-        try {
-            URL homeDir = getClass().getResource("/public/index.html");
-            URI homeUri = homeDir.toURI().resolve("./").normalize();
-
-            ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
-            context.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
-            ServletHolder holderPwd = new ServletHolder("default", WebjarDefaultServlet.class);
-            holderPwd.setInitParameter("dirAllowed", "true");
-            holderPwd.setInitParameter("resourceBase", homeUri.toString());
-            context.addServlet(holderPwd, "/");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+        ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+        context.setInitParameter("dirAllowed", "true");
+        context.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
+        ServletHolder holderPwd = new ServletHolder("default", WebjarDefaultServlet.class);
+        holderPwd.setInitParameter("dirAllowed", "true");
+        holderPwd.setInitParameter("resourceBase", this.getClass().getResource("/public").toExternalForm());
+        context.addServlet(holderPwd, "/");
         try {
             server.start();
             server.join();
